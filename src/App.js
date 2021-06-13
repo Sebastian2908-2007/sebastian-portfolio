bb398948ba6e377fef6fe6a50e8fccd8557794a8
sebastian <sebastian bowen>
2021-06-13 00:14:02 -0600

    about me is at mvp status

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import About from "./components/About";
 import Nav from './components/Nav';
-
+import Resume from "./components/Resume";
 function App() {
   return (
     <div>
      <Nav></Nav>
      <main>
        <About></About>
+       <Resume />
      </main>
     </div>
   );
---
+++ src/components/Resume/index.js
@@ -0,0 +1,12 @@
+
+
+function Resume() {
+    return (
+        <div>
+            <a download href="../../../docs/resume.pdf">Click here for my Resume</a>
+            
+        </div>
+    )
+}
+
+export default Resume;
